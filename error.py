1d37d7688b2c770c335ba5cff588f34b661d0a4e
ThreshvsGaming <135454181+ThreshvsGaming@users.noreply.github.com>
2026-05-25 12:58:37 -0500

    Add cryptographic algorithms with vulnerability notes
    Added cryptographic algorithm examples and comments on vulnerabilities.

--- error.py
+++ error.py
@@ -1,10 +1,22 @@
-def check_age(age):
-    if age < 0:
-        # Manually raising a ValueError with a custom message
-        raise ValueError("Age cannot be negative.")
-    return f"Age is {age}"
+# Strong cipher algorithms are cryptographic systems resistant to 
+# cryptanalysis, they are not vulnerable to well-known attacks like 
+# brute force attacks for example.
 
-try:
-    print(check_age(-5))
-except ValueError as fault:
-    print(f"Caught an error: {fault}")
+# It’s not recommended to use algorithm with a block size inferior 
+# than 128 bits.
+
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+key = os.urandom(16)
+iv = os.urandom(16)
+
+tdes4 = Cipher(algorithms.TripleDES(key), mode=None, backend=default_backend()) # Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
+bf3 = Cipher(algorithms.Blowfish(key), mode=None, backend=default_backend()) # Noncompliant: Blowfish use a 64-bit block size makes it vulnerable to birthday attacks
+rc42 = Cipher(algorithms.ARC4(key), mode=None, backend=default_backend()) # Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security
+
+    # OWASP Top 10 2021 Category A2 - Cryptographic Failures
+    # OWASP Top 10 2017 Category A6 - Security Misconfiguration
+    # MITRE, CWE-327 - Use of a Broken or Risky Cryptographic Algorithm
+    # SANS Top 25 - Porous Defenses
